8cea14321f72fcf4977647368ed6301066c7dfec
TVQ <tavanquyet2206@gmail.com>
2024-02-12 21:29:29 +0700

    thay doi db cloud

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect(`${process.env.CONNECT}`);
+        // await mongoose.connect(`${process.env.CONNECT}`);
+        await mongoose.connect(`${process.env.CONNECT_CLOUD}`);
         console.log('connect successffly');
     } catch (error) {
         console.log('connect fal');
